40e8f1fc0e4ae4fb4c70dd52fcf3168d8ff54f75
alvahdean <alvahdean@gmail.com>
2018-12-15 23:21:35 -0500

    Configuration binding

---
+++ Starbender.Romi.Services.Configuration/SupportedDevice.cs
@@ -0,0 +1,12 @@
+﻿namespace Starbender.Romi.Services.Configuration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+
+    public class SupportedDevice
+    {
+        public string Name { get; set; }
+    }
+}
