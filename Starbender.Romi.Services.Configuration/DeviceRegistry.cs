c19349ce1ff29ab91047b94e196455e8413b3cad
Dean Fuqua <alvahdean@gmail.com>
2018-12-16 01:14:26 -0500

    Configuration binding (#2)

---
+++ Starbender.Romi.Services.Configuration/DeviceRegistry.cs
@@ -0,0 +1,23 @@
+﻿namespace Starbender.Romi.Services.Configuration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Configuration object containing the device interfaces and
+    /// implemented devices that are supported in the web service
+    /// </summary>
+    public class DeviceRegistry
+    {
+        /// <summary>
+        /// Supported interfaces
+        /// </summary>
+        public List<SupportedInterface> SupportedInterfaces { get; set; }
+
+        /// <summary>
+        /// Supported devices
+        /// </summary>
+        public List<SupportedDevice> SupportedDevices { get; set; }
+    }
+}
---
+++ Starbender.Romi.Services.Configuration/SupportedInterface.cs
@@ -0,0 +1,22 @@
+﻿namespace Starbender.Romi.Services.Configuration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Class containing registration information for a device interface to be supported by the web service
+    /// </summary>
+    public class SupportedInterface
+    {
+        /// <summary>
+        /// Device interface name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Device interface version
+        /// </summary>
+        public int Version { get; set; }
+    }
+}
