30233311e3ec66f2b671c59d2785ab32ffa0b71f
Dean Fuqua <alvahdean@gmail.com>
2018-12-18 02:59:29 -0500

    Fix reference errors (#11)

--- Starbender.Romi.Services.Configuration/DependencyInjection.cs
+++ Starbender.Romi.Services.Configuration/DependencyInjection.cs
@@ -7,8 +7,7 @@
 
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Identity;
-    using Microsoft.EntityFrameworkCore;
+ 
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
