e50441295aa4a1ea135613979b544bdd0a2a6581
alvahdean <alvahdean@gmail.com>
2018-12-18 01:41:39 -0600

    Define AdministrationService

---
+++ Starbender.Romi.Services.Administration/IAdministrationService.cs
@@ -0,0 +1,27 @@
+﻿using Starbender.Romi.Data.Models;
+using Starbender.Romi.Services.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Starbender.Romi.Services.Administration
+{
+    public interface IAdministrationService
+    {
+        ServiceResult<RomiSettings> GetSettings();
+        ServiceResult<RomiSettings> UpdateSettings(RomiSettings settings);
+
+        ServiceResult<SupportedDevice> AddDevice(SupportedInterface deviceInterface, SupportedDevice device);
+        ServiceResult RemoveDevice(SupportedDevice device);
+        ServiceResult<IEnumerable<SupportedDevice>> GetDevices();
+
+        ServiceResult<SupportedInterface> AddInterface(SupportedInterface deviceInterface);
+        ServiceResult RemoveDevice(SupportedInterface deviceInterface);
+        ServiceResult<IEnumerable<SupportedInterface>> GetInterfaces();
+
+        ServiceResult<ApplicationIdentity> AddUser(ApplicationIdentity user);
+        ServiceResult RemoveUser(ApplicationIdentity user);
+        ServiceResult UpdateUser(ApplicationIdentity user);
+        ServiceResult<IEnumerable<ApplicationIdentity>> GetUsers();
+    }
+}
