0eb87704b148820c90358770df7e8f97dfc6bd1b
Dean Fuqua <alvahdean@gmail.com>
2018-12-18 02:23:14 -0500

    Create core service objects (#9)

---
+++ Starbender.Romi.Services.Core/ServiceResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Starbender.Romi.Services.Core
+{
+    public class ServiceResult
+    {
+        public static ServiceResult Failure => new ServiceResult(ServiceResultCode.Failure, "An error occured");
+
+        public static ServiceResult Success => new ServiceResult(ServiceResultCode.Success,null);
+
+        public ServiceResult()
+        {
+            Code = ServiceResultCode.Undefined;
+            ErrorMessage = null;
+        }
+
+        public ServiceResult(ServiceResultCode code,string errorMessage=null)
+        {
+            Code = code;
+            ErrorMessage = errorMessage;
+        }
+
+        public ServiceResultCode Code { get; set; }
+
+        private string ErrorMessage { get; set; }
+    }
+}
