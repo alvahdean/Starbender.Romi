7cb33c3d2c66e060fc0bef1bacb993e3e0c36aaf
alvahdean <alvahdean@gmail.com>
2018-12-24 16:18:16 -0500

    Property models

---
+++ Starbender.Romi.Data.Core/Models/InterfaceProperty.cs
@@ -0,0 +1,25 @@
+﻿namespace Starbender.Romi.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+
+    public class InterfaceProperty
+    {
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary> 
+        /// Gets or sets the interface that implements this property 
+        /// </summary>
+        public RegisteredInterface Interface { get; set; }
+
+        /// <summary> 
+        /// Gets or sets the property name 
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
--- Starbender.Romi.Data.Core/Models/RegisteredDevice.cs
+++ Starbender.Romi.Data.Core/Models/RegisteredDevice.cs
@@ -13,6 +13,15 @@
         public int Id { get; set; }
 
         /// <summary>
+        /// The application host that manages this device
+        /// </summary>
+        public RomiApplicationHost ApplicationHost { get; set; }
+
+        /// <summary>
+        /// The device interface that this device implements
+        /// </summary>
+        public RegisteredInterface Interface { get; set; }
+        /// <summary>
         /// Device Name
         /// </summary>
         public string Name { get; set; }
