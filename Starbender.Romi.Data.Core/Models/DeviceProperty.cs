cdb7c7486761b64397099e4ff2190b2b90417f25
Dean Fuqua <alvahdean@gmail.com>
2018-12-28 01:01:47 -0500

    Property models (#16)
    * Property models
    
    * Remove interfaces from application host

---
+++ Starbender.Romi.Data.Core/Models/DeviceProperty.cs
@@ -0,0 +1,30 @@
+﻿namespace Starbender.Romi.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+
+    public class DeviceProperty
+    {
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The device interface that this property is associated with
+        /// </summary>
+        public RegisteredDevice Device { get; set; }
+
+        /// <summary>
+        /// Interface property
+        /// </summary>
+        public InterfaceProperty Property { get; set; }
+
+        /// <summary>
+        /// Property Value
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
